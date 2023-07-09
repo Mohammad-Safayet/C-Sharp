47828e4213c82db57ff67fad5f7b265d9e49ef43
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-07-09 11:09:57 +0600

    added loops folder

---
+++ loops/loops/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6 };
+
+            // for loop 
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+            }
+            Console.WriteLine();
+
+            // ForEach loop
+            foreach (int i in arr)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+
+            // while loop
+            int index = 0;
+            while (index < arr.Length)
+            {
+                Console.Write(arr[index]);
+                index++;
+            }
+        }
+    }
+}
