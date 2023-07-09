a2bcc012e7027fb21dff9d090a5c0bdc5bc87dfb
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-07-09 11:09:28 +0600

    added array practice

---
+++ array/array/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ArrayTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, };
+            Console.WriteLine(nums.Length);
+
+            Console.WriteLine("First element in the nums: {0}", nums.First());
+            Console.WriteLine("Last element in the nums: {0}", nums.Last());
+            
+            Console.WriteLine("---------------------------------------------");
+
+            object[] objects = { "Bob", 30.14D, 652, 65.33256M, true};
+            for (int i = 0; i < objects.Length; i++)
+            {
+                Console.WriteLine("type: {0}\tValue: {1}\n",objects[i].GetType(), objects[i]);
+            }
+
+            Console.WriteLine("---------------------------------------------");
+
+            string[,] values = { { "Bob", "Jones" }, { "Joe", "Biden" } };
+
+            Console.WriteLine(values[1,0]);
+        }
+    }
+}
