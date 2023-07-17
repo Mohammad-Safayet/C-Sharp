924d4562850f4349d027adb36da67d5a79b47487
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-07-17 13:44:35 +0600

    added type enum

---
+++ enumerated-types/enumerated-types/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EnumeratedTypes
+{
+    class Program
+    {
+        enum Gender
+        {
+            male,
+            female,
+        }
+
+        enum DevPosition: byte
+        {
+            intern = 10,
+            junior,
+            senior,
+        }
+        static void Main(string[] args)
+        {
+            Gender gender = Gender.female;
+            Console.WriteLine("Enum is {0} with code {1}",gender, (int)gender);
+
+            DevPosition devPosition = DevPosition.senior;
+            Console.WriteLine("The position is {0} with code {1}",devPosition, (int)devPosition);
+        }
+    }
+}
