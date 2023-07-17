6a44d9a7f948f22cd59487f21e0c0ccacf81c0d1
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-07-17 13:43:30 +0600

    added practice null-type

---
+++ null-type/null-type/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace NullType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? num = null;
+            Console.WriteLine("is num is null? answer: {0}", num == null);
+            Console.WriteLine("does num has value? answer: {0}", num.HasValue);
+
+            num = 50;
+            Console.WriteLine("is num is null? answer: {0}", num == null);
+            Console.WriteLine("does num has value? answer: {0}", num.HasValue);
+        }
+    }
+}
