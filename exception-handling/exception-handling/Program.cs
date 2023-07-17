996eb117d763583a32426eee87ddfd3ea98c6b4a
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-07-17 13:46:01 +0600

    added practice exceptions

---
+++ exception-handling/exception-handling/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ExceptionHandling
+{
+    class Program
+    {
+        static int divide(int a, int b)
+        {
+            return a / b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exception Handeling");
+            int num1 = 10;
+            int num2 = 0;
+
+            try
+            {
+                int result = divide(num1, num2);
+            }
+            catch (DivideByZeroException excption)
+            {
+                Console.WriteLine(excption.Message);
+            } finally { Console.WriteLine("the final message is fix the code"); }
+        }
+    }
+}
