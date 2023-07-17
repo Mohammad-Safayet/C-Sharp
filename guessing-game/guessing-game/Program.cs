26917dc30b3567e1cc102303f7b9cd1ab758f61d
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-07-17 13:42:57 +0600

    added game practice

---
+++ guessing-game/guessing-game/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GuessingGame
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any key to start!");
+            Console.ReadLine();
+
+            Random random = new Random();
+            int result = random.Next(1, 51);
+            bool isSolved = false;
+
+            do
+            {
+                Console.WriteLine("Enter any number in between 1 - 51:");
+                int ans = Convert.ToInt32(Console.ReadLine());
+                isSolved = ans == result;
+
+            } while (!isSolved);
+        }
+    }
+}
