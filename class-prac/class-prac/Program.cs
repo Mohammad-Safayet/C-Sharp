ab5c7255e1763ce6d169a427ced92cc9e447dc0d
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-07-21 08:54:17 +0600

    practice class added

---
+++ class-prac/class-prac/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace class_prac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            Console.WriteLine("ID: {0}\nName: {1}\nDescription: {2}", student1.ID, student1.Name, student1.Description);
+        }
+
+    }
+}
---
+++ class-prac/class-prac/student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_prac
+{
+    class Student
+    {
+        public int ID;
+        public string Name;
+        public string Description;
+        
+        Student(int ID, string Name, string Description)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Description = Description;
+        }
+
+        public Student() {
+            this.ID = 0;
+            this.Name = "John Doe";
+            this.Description = "5'6'' white 155 pounds";
+        }
+
+        public Student(int ID, string Name)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Description = string.Empty;
+        }
+    }
+}
