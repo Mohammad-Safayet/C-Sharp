d3187d6fc48bb0a996781a9c79ed474208274274
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-07-25 07:00:27 +0600

    added inner class practice

---
+++ inner-class-prac/inner-class-prac/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace inner_class_prac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle.Owner owner = new Vehicle.Owner("Safayet Latif");
+            Vehicle vehicle = new Vehicle("Ferrari", owner);
+
+            vehicle.owner.getInfo();
+
+            Vehicle gmc = new Vehicle("GMC");
+            gmc.getInfo();
+        }
+    }
+}
---
+++ inner-class-prac/inner-class-prac/Vehicle.cs
@@ -0,0 +1,46 @@
+﻿namespace inner_class_prac
+{
+    class Vehicle
+    {
+        public string brand;
+        public Owner owner;
+
+        public Vehicle(string brand) : this(brand, new Vehicle.Owner("")) { }
+
+        public Vehicle(string brand, Owner owner)
+        {
+            this.brand = brand;
+            this.owner = owner;
+        }
+
+        public void getInfo()
+        {
+            Console.WriteLine("Brand: {0}", brand);
+            owner.getInfo();
+        }
+
+        public class Owner
+        {
+            private readonly int id;
+            private string name;
+
+            public Owner(string name)
+            {
+                this.id = (new Random()).Next();
+                Name = name;
+            }
+
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+
+            public void getInfo()
+            {
+                Console.WriteLine("Owner: {0}\nID: {1}", Name, this.id);
+            }
+        }
+    }
+}
