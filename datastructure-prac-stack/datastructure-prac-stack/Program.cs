03b8d86fbc64e977ad3474b1b9ba5fa6ae50b696
Mohammad-Safayet <mohammad.safayet360@gmail.com>
2023-07-27 17:58:33 +0600

    added stack practice

---
+++ datastructure-prac-stack/datastructure-prac-stack/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace datastructure_prac_stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            Console.WriteLine(String.Join(",", stack.ToArray()));
+            Console.WriteLine("Popping: {0}", stack.Peek());
+            stack.Pop();
+            Console.WriteLine("Popping: {0}", stack.Peek());
+            stack.Clear();
+        }
+    }
+}
